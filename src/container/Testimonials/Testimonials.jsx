a84750f42a7b26168ea50a29b3d62aa2c8db0820
Got-crypto <pecpalmplay@gmail.com>
2022-12-29 23:54:10 +0200

    finished creating a testimonial section

--- src/container/Testimonials/Testimonials.jsx
+++ src/container/Testimonials/Testimonials.jsx
@@ -11,6 +11,10 @@ const Testimonials = () => {
   const [testimonials, setTestimonials] = useState([])
   const [currentIndex, setCurrentIndex] = useState(0)
 
+  const handleClick = index => {
+    setCurrentIndex(index)
+  }
+
   useEffect(()=> {
     const populateTestimonials = async () => {
       const query = '*[_type == "brands"]'
@@ -26,11 +30,50 @@ const Testimonials = () => {
     populateTestimonials()
   }, [])
 
+  const test = testimonials[currentIndex]
+
   return (
     <>
       {
-        test
+        testimonials.length && (
+          <>
+            <div className='app__testimonial-item app__flex'>
+              <img 
+                src={urlFor(test.imageUrl)}
+                alt="testimonial"
+              />
+              <div className='app__testimonial-content'>
+                <p className='p-text'>{test.feedback}</p>
+                <div>
+                  <h4 className='bold-text'>{test.name}</h4>
+                  <h5 className='p-text'>{test.company}</h5>
+                </div>
+              </div>
+            </div>
+
+            <div className='app__testimonial-btns app__flex'>
+              <div className='app__flex' onClick={()=>handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+                <HiChevronLeft />
+              </div>
+              <div className='app__flex' onClick={()=>handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+                <HiChevronRight />
+              </div>
+            </div>
+          </>
+        )
       }
+
+      <div className='app__testimonial-brands app__flex'>
+        {brands.map(brand => (
+          <motion.div 
+            key={brand._id}
+            whileInView={{opacity: [0, 1]}}
+            transition={{duration: .5, type: 'tween'}}
+          >
+            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
     </>
   )
 }
