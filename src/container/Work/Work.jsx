8616b4046926e551dcd6e2048e6a1ee5fb004fa8
Got-crypto <pecpalmplay@gmail.com>
2022-12-29 05:23:04 +0200

    finished the portfolio apps section

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,12 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Work.scss'
 import { AppWrap } from '../../wrapper'
-
+import { AiFillEye, AiFillGithub } from 'react-icons/ai'
+import { motion } from 'framer-motion'
+import { urlFor, client } from '../../client'
+import { useEffect } from 'react'
 
 const Work = () => {
+
+  const [activeFilter, setActiveFilter] = useState('All')
+  const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1})
+  const [works, setWorks] = useState([])
+  const [filterWork, setFilterWork] = useState([])
+
+  const handleWorkFilter = item => {
+    setActiveFilter(item)
+    setAnimateCard([{y: 100, opacity: 0}])
+
+    setTimeout(() => {
+      setAnimateCard([{y: 0, opacity: 1}])
+
+      if( item === 'All' ){
+        setFilterWork(works)
+      }else {
+        setFilterWork(works.filter(work => work.tags.includes(item)))
+      }
+    }, 500);
+  }
+
+  useEffect(()=> {
+    const populateWorks = async () => {
+      const query = '*[_type == "works"]'
+
+      const data = await client.fetch(query)
+
+      setWorks(data)
+      setFilterWork(data)
+    }
+
+    populateWorks()
+  }, [])
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>my creative <span>portfolio </span>Section</h2>
+    
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map( (item, index) => {
+          return <div key={index} onClick={()=>handleWorkFilter(item)} className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}>
+            {item}
+          </div>
+        } )}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{duration: 0.5, delayChildren: 0.5}}
+        className="app__work-portfolio"
+      >
+        {filterWork.map(work=> {
+
+          console.log('work', work)
+
+          return <div className='app__work-item app__flex' key={work._id}>
+            <div className='app__work-img app__flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+
+              <motion.div
+                whileHover={{opacity: [0, 1]}}
+                transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileView={{scale: [0, 1]}}
+                    whileHover={{scale: [1, 0.9]}}
+                    transition={{duration: 0.25}}
+                    className='app__flex'
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileView={{scale: [0, 1]}}
+                    whileHover={{scale: [1, 0.9]}}
+                    transition={{duration: 0.25}}
+                    className='app__flex'
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+            <div className='app__work-content app__flex'>
+              <h4 className='bold-text'>{work.title}</h4>
+              <p className='p-text' style={{marginTop: 10}}>{work.description}</p>
+            
+              <div className='app__work-tag app__flex'>
+                <p className='p-text'>{work.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        })}
+      </motion.div>
+    </>
   )
 }
 
